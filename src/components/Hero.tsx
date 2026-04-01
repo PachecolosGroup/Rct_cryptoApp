6d244d1e2547c891d82c41219993a14992423d86
Pachecolosgroup <cesarppublicidad03@gmail.com>
2026-04-01 07:03:56 -0500

    Herosection
    Hero section, images, grid template, image, responsive.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,8 +1,31 @@
 import React from "react";
 import "./Hero.css";
+import Crypto from "../assets/venecuela.jpg";
 
 function Hero() {
-  return <div>Hero HERRRRRRRRRRRROOOOOOOOOOOOO</div>;
+  return (
+    <div className="hero">
+      <div className="container">
+        {/*Left side*/}
+        <div className="left">
+          <p>Welcome to the CryptoApp</p>
+          <h1>Invest in Cryptocurrency</h1>
+          <p>Buy, Sell and store hundreds of cryptocurrencies</p>
+          <div className="input-container">
+            <input type="email" placeholder="Enter your email" />
+            <button className="btn"> Learn More</button>
+          </div>
+        </div>
+        {/*Right side*/}
+
+        <div className="rigth">
+          <div className="img-container">
+            <img src={Crypto} alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Hero;
